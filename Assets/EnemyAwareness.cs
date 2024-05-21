e54a20551da8e55ba09ce60537dfdf77b15ca171
Albert Yeh <alyeh@cs.washington.edu>
2024-05-21 16:57:32 -0700

    Added basic following AI to enemy, added a scene for testing enemies, added a NavMesh component to the environment, and added the 'Player' tag to the player

--- Assets/EnemyAwareness.cs
+++ Assets/EnemyAwareness.cs
@@ -6,14 +6,17 @@ public class EnemyAwareness : MonoBehaviour
 {
     public Material aggroMat;
     public bool isAggro;
+    private Transform playerTransform;
+    private void Start() {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
     private void Update() {
+        if (Vector3.Distance(playerTransform.position, transform.position) <= 5f) {
+            isAggro = true;
+        }
+
         if (isAggro) {
             GetComponent<MeshRenderer>().material = aggroMat;
-        }
-    }
-    private void OnTriggerEnter(Collider other) {
-        if (other.transform.CompareTag("Player")) {
-            isAggro = true;
         }
     }
 }

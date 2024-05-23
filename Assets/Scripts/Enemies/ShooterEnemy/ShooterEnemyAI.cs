65951a20404248fff931eb19fa3fa3e482f13088
Albert Yeh <alyeh@cs.washington.edu>
2024-05-23 15:17:15 -0700

    ShooterEnemy now only fires when there is line of sight, and continues to follow player until it gets line of sight

--- Assets/Scripts/Enemies/ShooterEnemy/ShooterEnemyAI.cs
+++ Assets/Scripts/Enemies/ShooterEnemy/ShooterEnemyAI.cs
@@ -6,6 +6,8 @@ using UnityEngine.AI;
 public class ShooterEnemyAI : EnemyAI
 {
     [SerializeField] private float shootRange;
+    [SerializeField] private Transform shootPoint;
+    [SerializeField] private LayerMask projLayer;
     private EnemyShoot enemyShoot;
     protected override void Start() 
     {
@@ -14,8 +16,15 @@ public class ShooterEnemyAI : EnemyAI
     }
     protected override void Update()
     {
+        // Detect if player is in line of sight from the shooting point
+        bool playerInLOS = false;
+        if (Physics.Raycast(shootPoint.position, playerTransform.position - shootPoint.position, out RaycastHit hitInfo, shootRange, ~projLayer)) {
+            if (hitInfo.collider.tag == "PlayerModel") playerInLOS = true;
+        }
+
         if (enemyAwareness.isAggro && 
-            Vector3.Distance(playerTransform.position, transform.position) > shootRange) 
+            (Vector3.Distance(playerTransform.position, transform.position) > shootRange ||
+            (Vector3.Distance(playerTransform.position, transform.position) <= shootRange && !playerInLOS))) 
         {
             enemyNavMeshAgent.SetDestination(playerTransform.position);
         } else {

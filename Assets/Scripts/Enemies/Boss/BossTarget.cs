caa9ce967cf11b9a12b299f4476395dec55bd551
Jerry Li <jerryli1@cs.washington.edu>
2024-06-04 19:42:30 -0700

    sensitivity adjustments

--- Assets/Scripts/Enemies/Boss/BossTarget.cs
+++ Assets/Scripts/Enemies/Boss/BossTarget.cs
@@ -1,9 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.SearchService;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-
 public class BossTarget : Target
 {
     public ParticleSystem bloodParticles;
